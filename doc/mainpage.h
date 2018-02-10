6020fe67a43aa9943594f18b610ece501dd8b2ce
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2018-02-10 12:17:37 +0100

    Import of version 1.0.0

---
+++ doc/mainpage.h
@@ -0,0 +1,44 @@
+/**\mainpage ErrUt
+ *
+ * \author Jori Liesenborgs
+ *
+ * \section intro Introduction
+ *
+ * This is meant to be a set of simple error handling utilities. Currently
+ * it is only one header file. It was originally a part of a larger project,
+ * but turned out to be helpful in several other projects, which is why its
+ * own project was created.
+ *
+ * \section copyright Copyright license
+ * 	
+ * 	The library code uses the following MIT-style copyright license:
+ *
+ * 	\code
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation files
+ * (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY
+ * KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 	\endcode
+ *
+ * \section contact Contact
+ *
+ * 	If you have any questions, remarks or requests, you can contact me at
+ * 	\c jori(\c dot)\c liesenborgs(\c at)\c gmail(\c dot)\c com
+ *
+ */
+ 
